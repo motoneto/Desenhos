397b2a88bfdbf555fbf2ad84eabf9437184437ed
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-03-17 21:07:14 -0300

    Desenho G

---
+++ src/br/EXg.java
@@ -0,0 +1,21 @@
+package br;
+
+public class EXg implements Desenho {
+
+	@Override
+	public void desenhar(int SIZE) {
+		for (int i = 0; i < SIZE; i++){
+			for (int h = 0; h < SIZE; h++){
+				if (i == 0 || i == SIZE - 1)
+					System.out.print("# ");
+				else {
+					if (h == (SIZE - (i + 1)))
+						System.out.print("# ");
+					else 
+						System.out.print("  ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+}

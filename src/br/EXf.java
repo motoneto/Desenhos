1141884e3afac380ac3af32ac30bd4af1386cf6f
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-03-17 21:07:03 -0300

    Desenho F

---
+++ src/br/EXf.java
@@ -0,0 +1,22 @@
+package br;
+
+public class EXf implements Desenho {
+
+	@Override
+	public void desenhar(int SIZE) {
+		for (int i = 0; i < SIZE; i++){
+			for (int h = 0; h < SIZE; h++){
+				if (i == 0 || i == SIZE - 1)
+					System.out.print("# ");
+				else {
+					if (h == i)
+						System.out.print("# ");
+					else 
+						System.out.print("  ");
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
+}

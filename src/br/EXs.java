57b9bc3ee3dd56b1a2b2cc98db773fccc76a502e
Mauricio Ouriques Toneto <matt_ouriques@hotmail.com>
2016-03-17 22:06:13 -0300

    Desenho S

---
+++ src/br/EXs.java
@@ -0,0 +1,25 @@
+package br;
+
+public class EXs implements Desenho {
+
+	@Override
+	public void desenhar(int SIZE) {
+		for (int l = 1; l <= SIZE; l++){
+			int x = 0;
+			for (int c = 1; c < SIZE * 2; c++){
+				if (c <= l || c > SIZE * 2 - l - 1)
+					if (c <= SIZE)
+						System.out.print(++x + " ");
+					else {
+						if (x == SIZE) x--; 
+						System.out.print(x-- + " ");
+					}
+				else
+					System.out.print("  ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
